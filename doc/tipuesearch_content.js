30a77e4f51a0c2e63f281e1408ae8e4133a2f76f
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2019-08-16 17:56:40 +0900

    2019/08/16 17:56 updated.

--- doc/tipuesearch_content.js
+++ doc/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages":[{"title":"Title","text":"sample","tags":"keras","url":"https://iwasakishuto.github.io/Kerasy/doc/sample.html","loc":"https://iwasakishuto.github.io/Kerasy/doc/sample.html"}]};
+var tipuesearch = {"pages":[{"title":"Unknown","text":"sample","tags":"keras","url":"https://iwasakishuto.github.io/Kerasy/doc/sample.html","loc":"https://iwasakishuto.github.io/Kerasy/doc/sample.html"}]};

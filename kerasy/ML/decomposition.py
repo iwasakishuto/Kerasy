d35e9ff77f82c415bac8ae1688538d4bbc4f22d5
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2019-10-08 20:52:37 +0900

    2019/10/08 20:52 updated.

--- kerasy/ML/decomposition.py
+++ kerasy/ML/decomposition.py
@@ -8,25 +8,13 @@ class PCA():
         self.mean = None
         self.S = None
 
-    def svd_flip(u, v):
-        """
-        Sign correction to ensure deterministic output from SVD.
-        Adjusts the columns of u and the rows of v such that the loadings in the
-        columns in u that are largest in absolute value are always positive.
-        """
-        max_abs_cols = np.argmax(np.abs(u), axis=0)
-        signs = np.sign(u[max_abs_cols, range(u.shape[1])])
-        u *= signs
-        v *= signs[:, np.newaxis]
-        return u,v
-
     def fit(self,X):
         if self.n_components is None:
             self.n_components = min(X.shape)
         N,D = X.shape
         # X_mean = np.mean(X, axis=0) # X_ave.shape=(D,)
         # X_centralized = X-X_mean    # X_centralized.shape=(N,D)
-        S = np.cov(X.T)             # S[i][j] = np.mean(X_centralized[:,i]*X_centralized[:,j])
+        S = np.cov(X.T)               # S[i][j] = np.mean(X_centralized[:,i]*X_centralized[:,j])
         eigenvals, eigenvecs = np.linalg.eig(S)
         # NOTE: v[:,i] is the eigenvector corresponding to the eigenvalue w[i].
 

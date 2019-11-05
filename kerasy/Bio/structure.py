56faa5151aed0e82aa3f146acdf0bf3ee0786bd2
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2019-11-05 23:25:53 +0900

    2019/11/05 23:25 updated.

--- kerasy/Bio/structure.py
+++ kerasy/Bio/structure.py
@@ -115,10 +115,10 @@ class Nussinov(BaseHandler):
 
 class Zuker(BaseHandler):
     __name__ = "Zuker Algorithm"
-    __initialize_method__ = ['list2np', '_sort_stacking_cols', 'replaceINF']
-    __np_params__ = ["hairpin", "internal", "buldge", "stacking_score"]
-    __inf_params__ = ["hairpin", "internal", "buldge"]
-    __hidden_params__ = ["hairpin", "internal", "buldge", "stacking_cols", "stacking_score", "V", "M", "M1", "W", "sequence"]
+    __initialize_method__ = ['list2np', '_sort_stacking_cols', '_padding_stacking_score', 'replaceINF']
+    __np_params__     = ["hairpin", "internal", "buldge", "stacking_score"]
+    __inf_params__    = ["hairpin", "internal", "buldge"]
+    __hidden_params__ = ["hairpin", "internal", "buldge", "stacking_cols", "stacking_score", "V", "M", "W", "sequence", "bp"]
 
     def __init__(self):
         #=== parameter ===
@@ -139,11 +139,18 @@ class Zuker(BaseHandler):
         self.sequence = None
         self.W  = None # the minimum free energy of subsequence from i to j.
         self.V  = None # the minimum free energy of subsequence from i to j when i to j forms a base-pair.
-        self.M  = None # the minimum free energy of subsequence closed by two or more base pairs.
-        self.M1 = None # the minimum free energy of subsequence closed by one or more base pairs.
+        self.M  = None # the minimum free energy of subsequence when from i to j are in a multiloop and contain more than one base pairs which close it.
+        # TraceBack.
+        self.bp = []
 
     def _sort_stacking_cols(self):
         self.stacking_cols = np.array(["".join(sorted(bp)) for bp in self.stacking_cols])
+
+    def _padding_stacking_score(self):
+        N = len(self.stacking_score)
+        tmp = np.full(shape=(N+1,N+1), fill_value=self.inf)
+        tmp[:N,:N] = self.stacking_score
+        self.stacking_score = tmp
 
     def _calW(self,i,j):
         return min(
@@ -156,36 +163,39 @@ class Zuker(BaseHandler):
     def _calV(self,i,j):
         return min(
             self._F1(i,j),
-            min([self.inf]+[self._F2(i,j,h,l)+self.V[h][l] for h in range(i+1,j) for l in range(h+1,j) if self._is_bp(self.sequence[h],self.sequence[l])]),
-            self.M[i+1][j-1] + self.a + self.b
+            min([self.inf]+[self._F2(i,j,h,l)+self.V[h][l] for h in range(i+1,j-1) for l in range(h+1,j) if self._is_bp(self.sequence[h],self.sequence[l])]),
+            min([self.inf]+[self.M[i+1][k] + self.M[k+1][j-1] for k in range(i+1,j-1)]) + self.a + self.b,
         ) if self._is_bp(self.sequence[i],self.sequence[j]) else self.inf
 
     def _calM(self,i,j):
-        return min([self.M1[i][k] + self.M1[k+1][j] for k in range(i,j)])
-
-    def _calM1(self,i,j):
         return min(
-            self.M[i][j],
-            self.V[i][j]+self.b,
-            self.M1[i+1][j]+self.c,
-            self.M1[i][j-1]+self.c
+            self.V[i][j] + self.b,
+            self.M[i+1][j] + self.c,
+            self.M[i][j-1] + self.c,
+            min([self.inf]+[self.M[i][k] + self.M[k+1][j] for k in range(i,j)])
         )
 
     def _F1(self,i,j):
-        nt=j-i-1
+        """ i...j → length=3 → nt=2 (0-origin) """
+        nt=(j-i-1)-1 # 0-origin.
         return self.hairpin[nt] if nt<30 else self.inf
 
     def _F2(self,i,j,h,l):
-        nt = (h-i-1)+(j-l-1)
-        if nt>=30:
-            val = self.inf
+        """ i..h - l...j → length=2+3=5 → nt=4 """
+        nt = (h-i-1)+(j-l-1)-1 # 0-origin.
+        if nt>=30: val = self.inf
         elif (h==i+1) and (l==j-1):
-            ridx=np.argmax(self.stacking_cols=="".join(sorted(self.sequence[i]  +self.sequence[j])))
-            cidx=np.argmax(self.stacking_cols=="".join(sorted(self.sequence[i+1]+self.sequence[j-1])))
+            """ stacking """
+            rcond = self.stacking_cols=="".join(sorted(self.sequence[i]  +self.sequence[j]))
+            ccond = self.stacking_cols=="".join(sorted(self.sequence[i+1]+self.sequence[j-1]))
+            ridx = np.argmax(rcond) if np.any(rcond) else -1
+            cidx = np.argmax(ccond) if np.any(ccond) else -1
             val = self.stacking_score[ridx][cidx]
         elif (i+1<h<l<j-1):
+            """ internal loop """
             val = self.internal[nt]
         else:
+            """ bulge loop """
             val = self.buldge[nt]
 
         return val
@@ -194,61 +204,65 @@ class Zuker(BaseHandler):
         """ calcurate DP matrix. (Recursion) """
         N=len(sequence)
         self.sequence = sequence
-        self.V  = np.full(shape=(N,N), fill_value=self.inf)
-        self.M  = np.full(shape=(N,N), fill_value=self.inf)
-        self.M1 = np.full(shape=(N,N), fill_value=self.inf)
-        self.W  = np.full(shape=(N,N), fill_value=self.inf)
+        self.V  = np.full(shape=(N,N), fill_value=self.inf, dtype=float)
+        self.M  = np.full(shape=(N,N), fill_value=self.inf, dtype=float)
+        self.W  = np.full(shape=(N,N), fill_value=0, dtype=float)
         for ini_i in reversed(range(N)):
             diff = N-ini_i
             for i in reversed(range(ini_i)):
                 j = i+diff
                 self.V[i][j]  = self._calV(i,j)
                 self.M[i][j]  = self._calM(i,j)
-                self.M1[i][j] = self._calM1(i,j)
                 self.W[i][j]  = self._calW(i,j)
 
         score = self.W[0][-1]
         pairs = self.traceback()
         self._printAlignment(score, self.sequence, pairs, width=60, xlabel="seq", ylabel="")
 
+    def tracebackV(self, i, j):
+        self.bp.append((i,j))
+        if self.V[i][j] == self._F1(i,j): pass
+        else:
+            for h in range(i+1,j-1):
+                for l in range(h+1,j):
+                    if self._is_bp(self.sequence[h],self.sequence[l]):
+                        if self.V[i][j] == self._F2(i,j,h,l) + self.V[h][l]:
+                            self.tracebackV(h,l)
+        for k in range(i+1,j-1):
+            if self.V[i][j] == self.M[i+1][k] + self.M[k+1][j-1] + self.a+self.b:
+                self.tracebackM(i+1,k)
+                self.tracebackM(k+1,j-1)
+
+    def tracebackM(self, i, j):
+        if self.M[i][j] == self.V[i][j] + self.b:
+            self.tracebackV(i,j)
+        elif self.M[i][j] == self.M[i+1][j] + self.c: self.tracebackM(i+1,j)
+        elif self.M[i][j] == self.M[i][j-1] + self.c: self.tracebackM(i,j-1)
+        else:
+            for k in range(i,j):
+                if self.M[i][j] == self.M[i][k] + self.M[k+1][j]:
+                    self.tracebackM(i,k)
+                    self.tracebackM(k+1,j)
+
     def traceback(self):
-        """trackback to find which bases form base-pairs."""
-        N=len(self.sequence)
-        bp = []
+        """ Main trackback to find which bases form base-pairs. """
+        N = len(self.sequence)
+        self.bp = []
         stack = [(0,N-1)]
         while(stack):
-            i,j = stack.pop(0)
+            i,j = stack.pop()
             if (i>=j): continue
-            elif self.W[i+1][j] == self.W[i][j]: stack.append((i+1,j))
-            elif self.W[i][j-1] == self.W[i][j]: stack.append((i,j-1))
-            elif self.V[i][j]   == self.W[i][j]:
-                bp.append((i,j))
-                if self.V[i][j] == self._F1(i,j): continue
-                if self.V[i][j] == self.M[i+1][j-1]+self.a+self.b:
-                    for k in range(i+1,j-1):
-                        if self.M1[i][k] + self.M1[k+1][j] == self.M[i+1][j-1]:
-                            stack.append((k+1,j))
-                            stack.append((i,k))
-                            break
-                else:
-                    flag=0
-                    for h in range(i+1,j):
-                        for l in range(h+1,j):
-                            if self._is_bp(self.sequence[h],self.sequence[l]):
-                                if self.V[i][j] == self._F2(i,j,h,l) + self.V[h][l]:
-                                    stack.append((h,l))
-                                    flag=1
-                                    break
-                        if flag: break
-
+            elif self.W[i][j] == self.W[i+1][j]: stack.append((i+1,j))
+            elif self.W[i][j] == self.W[i][j-1]: stack.append((i,j-1))
+            elif self.W[i][j] == self.V[i][j]: self.tracebackV(i,j)
             else:
                 for k in range(i,j):
-                    if self.W[i][k] + self.W[k+1][j] == self.W[i][j]:
+                    if self.W[i][j] == self.W[i][k]+self.W[k+1][j]:
                         stack.append((k+1,j))
                         stack.append((i,k))
                         break
 
         pairs = list("." * N)
-        for i,j in bp:
+        for c,(i,j) in enumerate(self.bp):
             pairs[i] = "("; pairs[j] = ")"
         return "".join(pairs)

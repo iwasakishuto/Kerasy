9fa6be84e57642b7c8cea8a55f1cc6e4148b3c37
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2019-11-21 18:56:58 +0900

    2019/11/21 18:56 updated.

---
+++ kerasy/datasets/mnist.py
@@ -0,0 +1,19 @@
+"""MNIST handwritten digits dataset.
+"""
+from __future__ import absolute_import
+
+from ..utils.generic_utils import get_file
+import numpy as np
+
+def load_data(path='mnist.npz'):
+    """Loads the MNIST dataset.
+    @params  path: (str)   path where to cache the dataset locally (relative to ~/.kerasy/datasets).
+    @returns       (tuple) Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+    """
+    path = get_file(path,
+                    origin='https://s3.amazonaws.com/img-datasets/mnist.npz',
+                    file_hash='8a61469f7ea1b51cbae51d4f78837e45')
+    with np.load(path, allow_pickle=True) as f:
+        x_train, y_train = f['x_train'], f['y_train']
+        x_test, y_test = f['x_test'], f['y_test']
+    return (x_train, y_train), (x_test, y_test)
--- kerasy/utils/generic_utils.py
+++ kerasy/utils/generic_utils.py
@@ -1,7 +1,52 @@
+import os
 import sys
+from six.moves.urllib.request import urlretrieve
 from collections import defaultdict
 
 _UID_PREFIXES = defaultdict(int)
+
+def get_file(fname, origin, cache_subdir='datasets', file_hash=None):
+    """Downloads a file from a URL if not already in the cache.
+    ref: https://github.com/keras-team/keras/blob/7a39b6c62d43c25472b2c2476bd2a8983ae4f682/keras/utils/data_utils.py#L123
+
+    By default the file at the url `origin` is downloaded to the
+    cache_dir `~/.kerasy`, placed in the cache_subdir `datasets`,
+    and given the filename `fname`. The final location of a file
+    `example.txt` would therefore be `~/.kerasy/datasets/example.txt`.
+
+    You have to make a directory `~/.kerasy` and `~./kerasy/datasets`,
+    and check whether you can access these directories using `os.access("DIRECOTRY", os.W_OK)`
+    If this method returns False, you have to change the ownership of them like
+    ```
+    $ sudo chown iwasakioshuto: ~/.kerasy
+    $ sudo chown iwasakioshuto: ~/.kerasy/datasets
+    ```
+    """
+    cache_dir = os.path.join(os.path.expanduser('~'), '.kerasy')
+    datadir_base = os.path.expanduser(cache_dir)
+    if not os.access(datadir_base, os.W_OK):
+        datadir_base = os.path.join('/tmp', '.kerasy')
+    datadir = os.path.join(datadir_base, cache_subdir)
+    if not os.path.exists(datadir):
+        os.makedirs(datadir)
+    fpath = os.path.join(datadir, fname)
+
+    if not os.path.exists(fpath):
+        print('Downloading data from', origin)
+        error_msg = 'URL fetch failure on {} : {} -- {}'
+        try:
+            try:
+                urlretrieve(origin, fpath)
+            except HTTPError as e:
+                raise Exception(error_msg.format(origin, e.code, e.msg))
+            except URLError as e:
+                raise Exception(error_msg.format(origin, e.errno, e.reason))
+        except (Exception, KeyboardInterrupt):
+            if os.path.exists(fpath):
+                os.remove(fpath)
+            raise
+    return fpath
+
 
 def get_uid(prefix=""):
     _UID_PREFIXES[prefix] += 1

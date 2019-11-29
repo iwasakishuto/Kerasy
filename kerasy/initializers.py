791e705a84cdf50681b9ca881f8bfd0603e5c461
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2019-11-29 09:15:57 +0900

    2019/11/29 09:15 updated.

--- kerasy/initializers.py
+++ kerasy/initializers.py
@@ -28,13 +28,15 @@ def TruncatedNormal(shape, mean=0.0, stddev=0.05, seed=None):
     return X.rvs(size=shape,random_state=seed)
 
 def VarianceScaling(shape, scale=1.0, mode='fan_in', distribution='normal', seed=None):
-    n = "入力ユニットの数" if mode=="fan_in" else "出力ユニットの数" if mode=="fan_out" else "入力ユニットと出力ユニットの数の平均"
-
+    fan_in, fan_out = _compute_fans(shape)
+    n = fan_in if mode=="fan_in" else fan_out if mode=="fan_out" else (fan_in+fan_out)/2
+    scale /= max(1., n)
     if distribution=='normal':
-        stddev = np.sqrt(scale / n)
+        # 0.879... = scipy.stats.truncnorm.std(a=-2, b=2, loc=0., scale=1.)
+        stddev = np.sqrt(scale) / .87962566103423978
         return TruncatedNormal(shape=shape, mean=0.0, stddev=stddev, seed=seed)
     else:
-        limit = np.sqrt(3 * scale / n)
+        limit = np.sqrt(3 * scale)
         return RandomUniform(shape=shape, minval=-limit, maxval=limit, seed=seed)
 
 def Orthogonal(shape, gain=1.0, seed=None):
@@ -111,6 +113,36 @@ def LeCunUniform(shape, seed=None):
         distribution='uniform',
         seed=seed
     )
+
+def _compute_fans(shape, data_format='channels_last'):
+    """Computes the number of input and output units for a weight shape.
+    @param shape      : Integer shape tuple.
+    @param data_format: Image data format to use for convolution kernels.
+    @return fan_in    : size of the input shape.
+    @return fan_out   : size of the output shape.
+    """
+    if len(shape) == 2:
+        fan_in,fan_out = shape
+    elif len(shape) in {3, 4, 5}:
+        # Assuming convolution kernels (1D, 2D or 3D).
+        # TH kernel shape: (depth, input_depth, ...)
+        # TF kernel shape: (..., input_depth, depth)
+        if data_format == 'channels_first':
+            receptive_field_size = np.prod(shape[2:])
+            fan_in = shape[1] * receptive_field_size
+            fan_out = shape[0] * receptive_field_size
+        elif data_format == 'channels_last':
+            receptive_field_size = np.prod(shape[:-2])
+            fan_in = shape[-2] * receptive_field_size
+            fan_out = shape[-1] * receptive_field_size
+        else:
+            raise ValueError('Invalid data_format: ' + data_format)
+    else:
+        # No specific assumptions.
+        fan_in = np.sqrt(np.prod(shape))
+        fan_out = np.sqrt(np.prod(shape))
+    return fan_in, fan_out
+
 
 InitializeHandler = {
     'zeros': Zeros,

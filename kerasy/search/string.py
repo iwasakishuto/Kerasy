78707a8982c79e6e9917d640767e33c840cde2b1
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2019-12-14 16:51:16 +0900

    Add String Search DB.

--- kerasy/search/string.py
+++ kerasy/search/string.py
@@ -1,22 +1,47 @@
 # coding: utf-8
 import numpy as np
 from ..utils import CategoricalEncoder
+from ..utils import printAlignment
 
-class SuffixArray():
+def unzipBWT(BWT):
+    
+
+class StringSearch():
     def build(self, string):
+        if "$" in string:
+            raise TypeError(
+                "You do not need to add '$' at the end.",
+                "If '$' is used in `string`, please replace it."
+            )
         self.encoder = CategoricalEncoder()
         self.string = string
-        self.SA = self.SAIS(
+        self.len = len(string)+1 # + len("$")=1
+
+        print("Building Suffix Array...")
+        self.SA = np.asarray(self.SAIS(
             list(self.encoder.to_categorical(string, origin=1)),
             isLMSsorted=False,
             LMS=None
-        )
+        ), dtype=int)
+        # Inverse Suffix Array.
+        self.ISA = np.zeros(shape=self.len, dtype=int)
+        for i in range(self.len): self.ISA[self.SA[i]] = i
+        # Burrows-Wheeler Transform
+        print("Building Auxiliary data structure for BWT...")
+        self._build_BWT()
+        self._build_C()
+        self._build_Occ()
+        # Longest-Common-Prefix
+        print("Building Longest-Common-Prefix Array...")
+        self._build_height()
 
-    def summary(self):
-        digit = len(str(self.SA[0]))
-        for pos in self.SA:
-            print(f"{pos:>{digit}}", self.string[pos:] + "$")
-
+    def SuffixArray(self):
+        """ Print Suffix Array. """
+        digit = max(len(str(self.len)), 2)
+        print(f"{'i':>{digit}} {'SA':>{digit}} {'H':>{digit}} Suffix")
+        print("-"*(digit*3+9))
+        for i,pos in enumerate(self.SA):
+            print(f"{i:>{digit}} {pos:>{digit}} {self.height[i]:>{digit}} {self.string[pos:]+'$'}")
 
     @staticmethod
     def SAIS(string, isLMSsorted=False, LMS=None):
@@ -119,8 +144,85 @@ class SuffixArray():
         if enc < LMSn:
             # If we couldn't order LMS correctly, sort LMS recursively.
             sub_string = [pLMS[e] for e in LMS if e<l-1] # We have to remove last symbol "$".
-            order = SuffixArray.SAIS(sub_string, isLMSsorted=False, LMS=None) # Sort LMS recursively.
-            LMS = [LMstring[i] for i in order]
+            order = StringSearch.SAIS(sub_string, isLMSsorted=False, LMS=None) # Sort LMS recursively.
+            LMS = [LMS[i] for i in order]
         else:
             LMS = [i for i in SA if isLMS[i]]
-        return SuffixArray.SAIS(string, isLMSsorted=True, LMS=LMS)
+        return StringSearch.SAIS(string, isLMSsorted=True, LMS=LMS)
+
+    def _build_BWT(self):
+        """ characters just to the left of the suffixes in the suffix array """
+        self.BWT = np.asarray(list(self.string+"$"))[self.SA-1]
+
+    def _build_C(self):
+        """ the total occurrence of a characters which is smaller than x in lexicographical order in BWT."""
+        self.C = {"$": 0}
+        tmp = 1
+        for cha in self.encoder.obj2cls.keys():
+            self.C[cha] = tmp
+            tmp += np.count_nonzero(self.BWT==cha)
+
+    def _build_Occ(self):
+        """ the total occurrence of a character x up to i-th in the BWT. """
+        num_characters = len(self.encoder.obj2cls.keys())
+        self.Occ = np.zeros(shape=(self.len+1, num_characters), dtype=int)
+        tmp = np.zeros(shape=num_characters, dtype=int)
+        for i,cha in enumerate(self.BWT):
+            if cha != "$": tmp[self.encoder.obj2cls[cha]-1] += 1
+            self.Occ[i] = tmp
+        # The reason why there are all $0$ array in the last is "Occ(x,-1) = 0"
+
+    def search(self, query):
+        """ Scan through string looking for a match to the query.
+        returning a all of the match indexes, or -1 if no match was found.
+        """
+        lb = 0;  ub=int(len(self.BWT)-1)
+        q = ""
+        for cha in reversed(query):
+            if cha not in self.encoder.obj2cls.keys(): return -1
+            q  = cha+q
+            lb = int(self.C[cha] + self.Occ[lb-1][self.encoder.obj2cls[cha]-1])
+            ub = int(self.C[cha] + self.Occ[ub][self.encoder.obj2cls[cha]-1] - 1)
+            if lb>ub:
+                return -1
+        return self.SA[lb:ub+1]
+
+    def where(self, query, width=60):
+        len_query  = len(query)
+        len_string = len(self.string)
+
+        positions  = self.search(query)
+        score = 0 if isinstance(positions, int) else len(positions)
+        pos_info   = list(" " * len_string)
+        if not isinstance(positions, int):
+            for pos in positions:
+                pos_info[pos:pos+len_query] = list("*" * len_query)
+        pos_info = "".join(pos_info)
+        printAlignment(
+            sequences=[self.string, pos_info],
+            indexes=[np.arange(len_string), np.arange(len_string)],
+            seqname=['S', ' '],
+            width=width,
+            score=score
+        )
+
+    def lcp(self,i,j):
+        """ Longest-Common-Prefix """
+        l=0
+        while (l+max(i,j)<self.len-1) and (self.string[i+l]==self.string[j+l]):
+            l+=1
+        return l
+
+    def _build_height(self):
+        """ Longest-Common-Prefix in Suffix Array """
+        self.height = np.zeros(shape=self.len, dtype=int)
+        h=0
+        for i in range(self.len):
+            j = self.ISA[i]
+            if j == self.len-1:
+                self.height[j] = -1
+                h = 0
+                continue
+            k = self.SA[j+1]
+            h=h-1 + self.lcp(i+h-1, k+h-1) if h>0 else self.lcp(i, k)
+            self.height[j] = h

a2efd1d6be17552265d2179ccb0699979e53b936
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-01-13 18:26:13 +0900

    Add k++ initialization.

--- kerasy/ML/EM.py
+++ kerasy/ML/EM.py
@@ -4,8 +4,8 @@ import scipy.stats as stats
 
 from ..utils import findLowerUpper
 from ..utils import flush_progress_bar
-from ..utils import euclid_distances
-from ..utils import pairwise_euclid_distances
+from ..utils import paired_euclidean_distances
+from ..utils import pairwise_euclidean_distances
 
 class BaseEMmodel():
     def __init__(self, K, random_state=None, metrics="euclid"):
@@ -29,7 +29,7 @@ class BaseEMmodel():
             idx = np.random.choice(data_idx)
             mu = X[idx, None]
             for k in range(1,self.K):
-                min_distance = np.apply_along_axis(lambda x:np.min(euclid_distances(x, mu)), 1, X)
+                min_distance = np.asarray([np.min([paired_euclidean_distances(x,m) for m in mu]) for x in X])
                 idx = np.random.choice(data_idx, p=min_distance/np.sum(min_distance))
                 mu = np.r_[mu, X[idx,None]]
             self.mu = mu
@@ -79,7 +79,7 @@ class KMeans(BaseEMmodel):
             idx = self.Estep(X)
             if memorize: self.memorize_param(idx)
             self.Mstep(X, idx)
-            J = np.sum([np.sum(np.square(X[idx==k]-self.mu[k])) for k in range(self.K)])
+            J = np.sum(paired_euclidean_distances(X, self.mu[idx]))
             flush_progress_bar(it, max_iter, metrics=f"distortion measure: {J:.3f}", verbose=verbose)
             if it>0 and not np.any(pidx!=idx): break
             pidx=idx
@@ -118,14 +118,12 @@ class HamerlyKMeans(KMeans):
 
     def calcLowerBound(self, X, idx):
         """ Calculate the Lower Bounds for all data (Xi)
-        @param X          : shape=(N,D) 
+        @param X          : shape=(N,D)
         @param idx        : shape=(N,)
         @return second_min: shape=(N,) Minmum distance to the centroid which is not belong to.
         """
-        one = np.eye(self.K)
-        return np.asarray([
-            np.min(np.where(one[y]==1, np.inf, euclid_distances(x, self.mu))) for x,y in zip(X, idx)
-        ])
+        data2centroids = pairwise_euclidean_distances(X, self.mu)
+        return np.partition(data2centroids, kth=1, axis=1)[:,1]
 
     def Hamerly_initialize(self, X):
         """
@@ -139,7 +137,7 @@ class HamerlyKMeans(KMeans):
             self.num_cls[k]=np.count_nonzero(idx==k)
             self.sum[k]=np.sum(X[idx==k], axis=0)
         self.mu = self.sum/self.num_cls.reshape(-1,1)
-        self.upper=euclid_distances(X, self.mu[idx])
+        self.upper=paired_euclidean_distances(X, self.mu[idx])
         self.lower=self.calcLowerBound(X, idx)
         return idx
 
@@ -150,6 +148,7 @@ class HamerlyKMeans(KMeans):
         # Initialization.
         self.train_initialize(X, initializer=initializer)
         idx = self.Hamerly_initialize(X)
+        if memorize: self.memorize_param(idx)
         # EM algorithm.
         for it in range(max_iter):
             not_meet, new_idx = self.SparseEstep(X, idx)
@@ -160,7 +159,7 @@ class HamerlyKMeans(KMeans):
             # Update index.
             idx[not_meet] = new_idx
             self.updateUpperLower(idx, dmu)
-            flush_progress_bar(it, max_iter, metrics=f"changed: {len(changed):>0{dim}}/{N}", verbose=verbose)               
+            flush_progress_bar(it, max_iter, metrics=f"changed: {len(changed):>0{dim}}/{N}", verbose=verbose)
             if memorize: self.memorize_param(idx)
         if memorize: self.memorize_param(idx)
         if verbose: print()
@@ -173,24 +172,24 @@ class HamerlyKMeans(KMeans):
         @return not_meet_again: shape=(?,) Index of data that did not meet the Hamerly' Proposition
         @return new_idx       : shape=(?,) X[not_meet_again]'s new idx.
         """
-        pairwise_cent_dist = pairwise_euclid_distances(self.mu, squared=False)
+        pairwise_cent_dist = pairwise_euclidean_distances(self.mu, squared=False)
         pairwise_cent_dist += np.max(pairwise_cent_dist)*np.identity(self.K) # Add maximum to diagonal components.
-        nearest_cent_dist = np.min(pairwise_cent_dist, axis=0) 
+        nearest_cent_dist = np.min(pairwise_cent_dist, axis=0)
         harmerly_right_side = np.maximum(nearest_cent_dist[idx]/2, self.lower)
         """ Hamerly' Proposition step.1 """
         not_meet = np.nonzero(self.upper>harmerly_right_side)[0] # Index of Xi who does not meet the Hamerly's Proposition.
-        self.upper[not_meet] = euclid_distances(X[not_meet], self.mu[idx[not_meet]]) # Update the upper bound.
+        self.upper[not_meet] = paired_euclidean_distances(X[not_meet], self.mu[idx[not_meet]]) # Update the upper bound.
         """ Hamerly' Proposition step.2 """
         not_meet_again = not_meet[np.nonzero(self.upper[not_meet]>harmerly_right_side[not_meet])]
         # Apply Estep Only to `X[not_meet_again]`
         new_idx = self.Estep(X[not_meet_again])
         # Update lower bounds and upper bounds.
-        self.upper[not_meet_again] = euclid_distances(X[not_meet_again], self.mu[new_idx])
+        self.upper[not_meet_again] = paired_euclidean_distances(X[not_meet_again], self.mu[new_idx])
         self.lower[not_meet_again] = self.calcLowerBound(X[not_meet_again], new_idx)
-        
-        changed = np.nonzero(new_idx==idx[not_meet_again]) 
+
+        changed = np.nonzero(new_idx==idx[not_meet_again])
         return not_meet_again, new_idx
-    
+
     def Mstep(self, X, bid, aid):
         """Think about only changed data.
         @param X   : shape=(?, D)
@@ -205,15 +204,109 @@ class HamerlyKMeans(KMeans):
             self.num_cls[k] += np.count_nonzero(aid==k)
             self.sum[k]     += np.sum(X[aid==k], axis=0)
         mu = self.sum/self.num_cls.reshape(-1,1)
-        dmu = euclid_distances(mu, self.mu)
+        dmu = paired_euclidean_distances(mu, self.mu)
         self.mu = mu
         return dmu
-    
+
     def updateUpperLower(self, idx, dmu):
         """ Update lower bounds and upper bounds. """
         max_dmu = np.max(dmu)
         self.upper += dmu[idx]
         self.lower -= max_dmu
+
+class ElkanKMeans(KMeans):
+    def __init__(self, K, random_state=None):
+        super().__init__(K=K, random_state=random_state)
+        self.lower=None   # shape=(N,K), Lower bound of distance to all centoroids.
+        self.upper=None   # shape=(N), Upper bound of distance to nearest centroid.
+        self.tight=None   # shape=(N), Whether Upper bounds are tight or not.
+        self.num_cls=None # shape=(K), Number of data belonging to class k.
+        self.sum=None     # shape=(K,D)
+        # self.mu=None      # shape=(K,D) mu = sum/num_cls
+
+    def Elkan_initialize(self, X):
+        N,D = X.shape
+        idx = self.Estep(X)
+        self.num_cls=np.zeros(shape=self.K)
+        self.sum=np.zeros(shape=(self.K, D))
+        for k in np.arange(self.K):
+            self.num_cls[k]=np.count_nonzero(idx==k)
+            self.sum[k]=np.sum(X[idx==k], axis=0)
+        self.mu = self.sum/self.num_cls.reshape(-1,1)
+        self.upper=paired_euclidean_distances(X, self.mu[idx])
+        self.lower=np.asarray([paired_euclidean_distances(x, self.mu) for x in X])
+        self.tight=np.ones(shape=N, dtype=bool)
+        return idx
+
+    def fit(self, X, max_iter=100, memorize=False, initializer="uniform", verbose=1):
+        """ @param X: shape=(N,D) """
+        N,D = X.shape
+        dim = len(str(N))
+        # Initialization.
+        self.train_initialize(X, initializer=initializer)
+        idx = self.Elkan_initialize(X)
+        if memorize: self.memorize_param(idx)
+        # EM algorithm.
+        for it in range(max_iter):
+            changed,idx = self.SparseEstep(X, idx)
+            if changed==0: break
+            # Update centroid.
+            dmu = self.Mstep()
+            self.updateUpperLower(idx, dmu)
+            flush_progress_bar(it, max_iter, metrics=f"changed: {len(changed):>0{dim}}/{N}", verbose=verbose)
+            if memorize: self.memorize_param(idx)
+        if memorize: self.memorize_param(idx)
+        if verbose: print()
+
+    def SparseEstep(self, X, idx):
+        changed = 0
+        # 1) For all clusters c and c', compute d(c,c').
+        pairwise_center_half_dist = 1/2 * pairwise_euclidean_distances(self.mu, squared=False)
+        # For all centers, compute s(c) = 1/2 min_{c'≠c}d(c,c')
+        nearest_center_dist = np.partition(pairwise_center_half_dist, kth=1, axis=0)[1]
+        for i,x in enumerate(X):
+            ub = self.upper[i]
+            cls = idx[i]
+            # 2) This means that the nearlest center is far away from the currently assigned center.
+            if nearest_center_dist[cls] >= ub:
+                continue
+
+            for k in range(self.K):
+                # 3. If this hold, then k is a good candidate fot the sample to be relabeled.
+                if (k!=cls and (ub > self.lower[i,k]) and (ub > pairwise_center_half_dist[k,cls])):
+                    # 3.a Recomputing the actual distance between sample and label.
+                    if not self.tight[i]:
+                        ub = paired_euclidean_distances(x,self.mu[cls])
+                        self.lower[i,cls] = ub
+                        self.tight[i] = True
+
+                    # 3.b
+                    if (ub>self.lower[i,k] or ub>pairwise_center_half_dist[cls,k]):
+                        dist = paired_euclidean_distances(x,self.mu[k])
+                        self.lower[i,k] = dist
+                        if dist < ub:
+                            self.num_cls[cls] -= 1; self.sum[cls] -= x
+                            self.num_cls[k] += 1; self.sum[k] += x
+                            cls = k
+                            ub = distance
+            self.upper[i] = ub
+            if idx[i] != cls:
+                changed += 1
+                idx[i]   = cls
+        return changed,idx
+
+    def Mstep(self):
+        mu  = self.sum/self.num_cls.reshape(-1,1)
+        dmu = paired_euclidean_distances(mu, self.mu)
+        self.mu = mu
+        return dmu
+
+    def updateUpperLower(self, idx, dmu):
+        """ Update lower bounds and upper bounds. """
+        self.lower = np.maximum(self.lower-dmu, 0)
+        self.upper += dmu[idx]
+        self.tight[:] = False
+
 class MixedGaussian(BaseEMmodel):
     def __init__(self, K, random_state=None):
         super().__init__(K=K, random_state=random_state)

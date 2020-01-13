907300019cbbaa9b226d8b35c9441d3bdb512d12
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-01-13 17:42:56 +0900

    2020/01/13 17:42 updated.

--- kerasy/utils/metric_utils.py
+++ kerasy/utils/metric_utils.py
@@ -2,32 +2,135 @@
 
 import numpy as np
 
+from ..utils import handleKeyError
+
+
+def norm_vectors(*args, axis=-1, squared=True):
+    if squared:
+        return tuple([np.sum(np.square(arg), axis=axis) for arg in args])
+    else:
+        return tuple([np.sqrt(np.sum(np.square(arg), axis=axis)) for arg in args])
+
+def normalize_vectors(*args, axis=-1):
+    return tuple([arg/np.expand_dims(np.linalg.norm(arg, axis=axis), axis=axis) for arg in args])
+
 def mean_squared_error(y_true, y_pred, sample_weight=None):
     return np.average((y_true - y_pred)**2, axis=0, weights=sample_weight).astype(float)
 
 def root_mean_squared_error(y_true, y_pred, sample_weight=None):
     return np.sqrt(mean_squared_error(y_true, y_pred, sample_weight=sample_weight))
 
-def euclid_distances(x,y):
+# Paired Distances
+def check_paired_array(X,Y):
+    """ Arrange the two array shapes.
+    @param  X: shape=(Nx,Dx)
+    @param  Y: shape=(Ny,Dy)
+    @return X: shape=(N,D)
+    @return Y: shape=(N,D)
     """
-    @param x,y shape=(N,D)
-    @return shape=(N,)
-    """
-    return np.sqrt(np.sum(np.square(x-y), axis=1))
+    if X.ndim==1: X=X.reshape(1,-1)
+    if Y.ndim==1: Y=Y.reshape(1,-1)
 
-def pairwise_euclid_distances(X, squared=False):
+    Nx,Dx = X.shape; Ny,Dy = Y.shape
+
+    if Dx != Dy:
+        raise ValueError(f"X.shape[1], and Y.shape[1] should be the same shape, but X.shape[1]={Dx} while Y.shape[1]={Dy}")
+    if Nx!=Ny:
+        if Nx==1:   x=np.repeat(x, Ny, axis=1)
+        elif Ny==1: y=np.repeat(y, Nx, axis=1)
+        else:
+            raise ValueError(f"X.shape={X.shape}, and Y.shape={Y.shape}, so we couldn't understand how to pair of 2 arrays.")
+    return X,Y
+
+def paired_distances(X,Y,metric="euclidean",**kwargs):
+    """Compute the paired distances between X and Y.
+    @params X,Y      : shape=(N,D) should be the same.
+    @param metric    : string or callable.
+    @return distances: shape=(N,)
+    """
+    if metric in PAIRED_DISTANCES:
+        func = PAIRED_DISTANCES[metric]
+        return func(X,Y)
+    elif callable(metric):
+        X,Y = check_paired_array(X,Y)
+        distances = np.asarray([metric(x,y) for x,y in zip(X,Y)])
+        return distances
+    else:
+        handleKeyError(list(PAIRED_DISTANCES.keys()), metric=metric)
+
+def paired_euclidean_distances(X,Y,squared=False):
+    X,Y = check_paired_array(X,Y)
+    if squared:
+        return np.sum(np.square(X-Y), axis=1)
+    else:
+        return np.sqrt(np.sum(np.square(X-Y), axis=1))
+
+def paired_manhattan_distances(X,Y):
+    X,Y = check_paired_array(X,Y)
+    return np.sum(np.abs(X,Y), axis=1)
+
+def paired_cosine_distances(X,Y):
+    """ ||X-Y||^2 = ||X||^2 + ||Y||^2 - 2||X||･||Y||cos(theta) """
+    X,Y = check_paired_arrays(X,Y)
+    X,Y = normalize_vectors(X,Y)
+    return .5 * paired_euclidean_distances(X-Y, squared=True)
+
+PAIRED_DISTANCES = {
+    'cosine': paired_cosine_distances,
+    'euclidean': paired_euclidean_distances,
+    'l2': paired_euclidean_distances,
+    'l1': paired_manhattan_distances,
+    'manhattan': paired_manhattan_distances,
+    'cityblock': paired_manhattan_distances,
+}
+
+# Pairwise Distances
+def check_pairwise_array(X,Y=None):
+    """ Arrange for the pairwise distance.
+    @param X: shape=(N,D)
+    """
+    if Y is None:
+        return X,X
+
+    if X.ndim==1: X=X.reshape(1,-1)
+    if Y.ndim==1: Y=Y.reshape(1,-1)
+    Nx,Dx = X.shape; Ny,Dy = Y.shape
+    if Dx != Dy:
+        raise ValueError(f"X.shape={X.shape}, and Y.shape={Y.shape}, so we couldn't understand how to pairwise of 2 arrays.")
+    return X,Y
+
+def pairwise_distances(X,Y=None,metric="euclidean",**kwargs):
+    """Compute the pair-wise distances between X and Y (X).
+    @params X        : shape=(Nx,D)
+    @params Y        : shape=(Ny,D)
+    @param metric    : string or callable.
+    @return distances: shape=(Nx,Ny)
+    """
+    if metric in PAIRWISE_DISTANCES:
+        func = PAIRWISE_DISTANCES[metric]
+        return func(X,Y,**kwargs)
+    elif metric in PAIRED_DISTANCES:
+        func = PAIRWISE_DISTANCES[metric]
+        distances = np.asarray([func(x,Y) for x in X])
+        return distances
+    elif callable(metric):
+        X,Y = check_pairwise_array(X,Y)
+        distances = np.asarray([[metric(x,y) for y in Y] for x in X])
+        return distances
+    else:
+        handleKeyError(list(PAIRWISE_DISTANCES.keys()), metric=metric)
+
+def pairwise_euclidean_distances(X,Y=None,squared=False):
     """Calculate euclid distance
-    @param X         : (ndarray) shape=(N,D)
-                        - N = Number of Data.
-                        - D = Number of features.
+    @param X: (ndarray) shape=(Nx,D)
+    @param Y: (ndarray) shape=(Ny,D)
     @param squared   : (bool) whether distances will be squared or not.
     @return distances: Dij means the distance between Xi and Xj.
     """
-    dot_product = np.dot(X, X.T) # dot product between all X
-    square_norm = np.diag(dot_product) # extract only diagonal elements so that get respective norms.
-
-    # ||a - b||^2 = ||a||^2  - 2 <a, b> + ||b||^2
-    distances = np.expand_dims(square_norm, axis=0) - 2.0 * dot_product + np.expand_dims(square_norm, axis=1)
+    X,Y = check_pairwise_array(X,Y)
+    Xnorm,Ynorm = l2norm_vectors(X,Y,axis=-1,squared=True)
+    # ||a - b||^2 = ||a||^2 + ||b||^2 - 2 <a, b>
+    distances = np.expand_dims(Xnorm, axis=1) + np.expand_dims(Ynorm, axis=0) - 2*X.dot(Y.T)
     distances = np.maximum(distances, 0.0) # for numerical errors.
 
     if not squared:
@@ -37,3 +140,8 @@ def pairwise_euclid_distances(X, squared=False):
         distances = distances * np.logical_not(mask) # Correct the epsilon added
 
     return distances
+
+PAIRWISE_DISTANCES = {
+    'euclidean': paired_euclidean_distances,
+    'l2': paired_euclidean_distances,
+}

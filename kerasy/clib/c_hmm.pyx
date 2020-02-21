5665494d802372e706804a6b5edbd745e133bb07
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-02-21 10:54:41 +0900

    2020/02/21 10:54 updated.

--- kerasy/clib/c_hmm.pyx
+++ kerasy/clib/c_hmm.pyx
@@ -10,122 +10,140 @@ from cython cimport view, floating
 from numpy.math cimport INFINITY
 from _cutils cimport c_argmax, c_max, c_logsumexp, c_logaddexp
 
-def _forward(
+# Log-scaled forward algorithm
+def _log_forward(
         np.ndarray[floating, ndim=1, mode='c'] log_initial_prob,
         np.ndarray[floating, ndim=2, mode='c'] log_transit_prob,
-        np.ndarray[floating, ndim=2, mode='c'] log_prob):
+        np.ndarray[floating, ndim=2, mode='c'] log_cond_prob):
     """ Forward Algorhtm for HMM. alpha(z_n) = p(x_1, ..., x_n, z_n)
     @params log_initial_prob : Log initial hidden states probabilities. shape=(n_hstates)
     @params log_transit_prob : Log transition probabilities. shape=(n_hstates, n_hstates)
-    @params log_prob         : Log probabilities. shape=(n_samples, n_hstates)
-    @return log_alpha        : Log shape=(n_samples, n_hstates)
+    @params log_cond_prob    : Log conditional probabilities. shape=(n_samples, n_hstates)
+    @return log_alpha        : Log scaled alpha. shape=(n_samples, n_hstates)
     """
     dtype = np.float32 if floating is float else np.float64
-    cdef int n_samples = log_prob.shape[0]
-    cdef int n_hstates = log_prob.shape[1]
+    cdef int n_samples = log_cond_prob.shape[0]
+    cdef int n_hstates = log_cond_prob.shape[1]
     cdef np.ndarray[floating, ndim=2, mode='c'] log_alpha = np.empty(shape=(n_samples, n_hstates), dtype=dtype)
     cdef np.ndarray[floating, ndim=1, mode='c'] buffer = np.empty(shape=(n_hstates), dtype=dtype)
 
-    cdef int n, i, j
+    cdef int n, i, k
     # (PRML 13.37) alpha(z_1) = p(x_1,z_1) = p(z_1)*p(x_1|z_1)
     for i in range(n_hstates):
-        log_alpha[0, i] = log_initial_prob[i] + log_prob[0, i]
+        log_alpha[0, i] = log_initial_prob[i] + log_cond_prob[0, i]
 
     for n in range(1, n_samples):
-        for j in range(n_hstates):
+        for k in range(n_hstates):
             for i in range(n_hstates):
-                buffer[i] = log_alpha[n-1, i] + log_transit_prob[i, j]
+                buffer[i] = log_alpha[n-1,i] + log_transit_prob[i,k]
             # (PRML 13.36) alpha(z_n) = p(x_n|z_n) sum(alpha(z_{n-1})*p(z_n|z_{n-1}))
-            log_alpha[n, j] = c_logsumexp(buffer) + log_prob[n, j]
+            log_alpha[n,k] = c_logsumexp(buffer) + log_cond_prob[n,k]
     return log_alpha
 
-def _backward(int n_samples, int n_hstates,
-              double[:] log_initial_prob,
-              double[:, :] log_transit_prob,
-              double[:, :] log_prob,
-              double[:, :] bwdlattice):
+# Log-scaled backward algorithm
+def _log_backward(
+        np.ndarray[floating, ndim=1, mode='c'] log_initial_prob,
+        np.ndarray[floating, ndim=2, mode='c'] log_transit_prob,
+        np.ndarray[floating, ndim=2, mode='c'] log_cond_prob):
+    """ Backward Algorhtm for HMM. beta(z_n) = p(x_{n+1}, ..., x_N | z_n)
+    @params log_initial_prob : Log initial hidden states probabilities. shape=(n_hstates)
+    @params log_transit_prob : Log transition probabilities. shape=(n_hstates, n_hstates)
+    @params log_cond_prob    : Log conditional probabilities. shape=(n_samples, n_hstates)
+    @return log_beta         : Log scaled beta. shape=(n_samples, n_hstates)
+    """
+    dtype = np.float32 if floating is float else np.float64
+    cdef int n_samples = log_cond_prob.shape[0]
+    cdef int n_hstates = log_cond_prob.shape[1]
+    cdef np.ndarray[floating, ndim=2, mode='c'] log_beta = np.empty(shape=(n_samples, n_hstates), dtype=dtype)
+    cdef np.ndarray[floating, ndim=1, mode='c'] buffer = np.empty(shape=(n_hstates), dtype=dtype)
 
-    cdef int t, i, j
-    cdef double[::view.contiguous] work_buffer = np.zeros(n_hstates)
+    cdef int n, i, k
+    # (PRML 13.39) p(z_N|X) = p(X,z_N)*beta(z_n) / p(X) ∴ beta[z_N]=1
+    for i in range(n_hstates):
+        log_beta[n_samples-1, i] = 0.0
 
-    with nogil:
-        for i in range(n_hstates):
-            bwdlattice[n_samples - 1, i] = 0.0
-
-        for t in range(n_samples - 2, -1, -1):
+    # (PRML 13.38) beta(z_n) = sum_{z_{n+1}}(beta(z_{n+1})*p(x_{n+1}|z_{n+1})*p(z_{n+1}|z_n))
+    for n in range(n_samples-2, -1, -1):
+        for k in range(n_hstates):
             for i in range(n_hstates):
-                for j in range(n_hstates):
-                    work_buffer[j] = (log_transit_prob[i, j]
-                                      + log_prob[t + 1, j]
-                                      + bwdlattice[t + 1, j])
-                bwdlattice[t, i] = c_logsumexp(work_buffer)
+                buffer[i] = log_transit_prob[k,i] + log_cond_prob[n+1,i] + log_beta[n+1,i]
+            log_beta[n,k] = c_logsumexp(buffer)
+    return log_beta
 
+# Log-scaled xi
+def _compute_log_xi_sum(
+        np.ndarray[floating, ndim=2, mode='c'] log_alpha,
+        np.ndarray[floating, ndim=2, mode='c'] log_beta,
+        np.ndarray[floating, ndim=2, mode='c'] log_transit_prob,
+        np.ndarray[floating, ndim=2, mode='c'] log_cond_prob):
+    """ Compute log-scaled xi(z_{n-1},z_n) from log-scaled alpha, and beta.
+    @params log_alpha        : Log scaled alpha. shape=(n_samples, n_hstates)
+    @params log_beta         : Log scaled beta.  shape=(n_samples, n_hstates)
+    @params log_transit_prob : Log transition probabilities. shape=(n_hstates, n_hstates)
+    @params log_cond_prob    : Log conditional probabilities. shape=(n_samples, n_hstates)
+    @return log_xi_sum       : Log scaled xi. ( xi.shape=(n_samples-1, n_hstates, n_hstates) )
+    """
+    dtype = np.float32 if floating is float else np.float64
+    cdef int n_samples = log_cond_prob.shape[0]
+    cdef int n_hstates = log_cond_prob.shape[1]
+    cdef np.ndarray[floating, ndim=2, mode='c'] log_xi_sum = np.full(shape=(n_hstates, n_hstates), fill_value=-INFINITY)
+    # (PRML 13.42) p(X) = sum_{z_N}(alpha(z_N))
+    cdef floating log_prob = c_logsumexp(log_alpha[n_samples-1])
 
-def _compute_log_xi_sum(int n_samples, int n_hstates,
-                        double[:, :] log_alpha,
-                        double[:, :] log_transit_prob,
-                        double[:, :] bwdlattice,
-                        double[:, :] log_prob,
-                        double[:, :] log_xi_sum):
-
-    cdef int t, i, j
-    cdef double[:, ::view.contiguous] work_buffer = \
-        np.full((n_hstates, n_hstates), -INFINITY)
-    cdef double logprob = c_logsumexp(log_alpha[n_samples - 1])
-
-    with nogil:
-        for t in range(n_samples - 1):
+    cdef int n, i, k
+    # (PRML 13.43) xi(z_{n-1},z_n) = alpha(z_{n-1})*p(x_n|z_n)*p(z_n|z_{n-1})*beta(x_n) / p(X)
+    for n in range(n_samples-1):
+        for k in range(n_hstates):
             for i in range(n_hstates):
-                for j in range(n_hstates):
-                    work_buffer[i, j] = (log_alpha[t, i]
-                                         + log_transit_prob[i, j]
-                                         + log_prob[t + 1, j]
-                                         + bwdlattice[t + 1, j]
-                                         - logprob)
+                # log-version "+="
+                log_xi_sum[k,i] = c_logaddexp(
+                    log_xi_sum[k,i],
+                    log_alpha[n,k] + log_cond_prob[n+1,i] + log_transit_prob[k,i] + log_beta[n+1,i] - log_prob
+                )
+    return log_xi_sum
 
+# Log-scaled viterbi algorithm.
+def _viterbi(
+         np.ndarray[floating, ndim=1, mode='c'] log_initial_prob,
+         np.ndarray[floating, ndim=2, mode='c'] log_transit_prob,
+         np.ndarray[floating, ndim=2, mode='c'] log_cond_prob):
+    """ Viterbi Algorhtm.
+    @params log_initial_prob : Log initial hidden states probabilities. shape=(n_hstates)
+    @params log_transit_prob : Log transition probabilities. shape=(n_hstates, n_hstates)
+    @params log_cond_prob    : Log conditional probabilities. shape=(n_samples, n_hstates)
+    @return ml_hstates       : Maximum likelihood hidden state sequences. shape=(n_samples)
+    @return log_prob         : Log scaled probability if the states were emitted from `ml_hstates`.
+    """
+    dtype = np.float32 if floating is float else np.float64
+    cdef int n_samples = log_cond_prob.shape[0]
+    cdef int n_hstates = log_cond_prob.shape[1]
+    cdef np.ndarray[floating, ndim=2, mode='c'] omega = np.zeros((n_samples, n_hstates), dtype=dtype)
+    cdef np.ndarray[int,      ndim=1, mode='c'] ml_hstates = np.empty(n_samples, dtype=np.int32)
+    cdef np.ndarray[floating, ndim=1, mode='c'] buffer = np.empty(shape=(n_hstates), dtype=dtype)
+    cdef int n, i, k, where_from
+
+    # Dynamic Programming
+    # Initialization
+    # (PRML 13.69) omega(z_1) = log(p(z_1)) + log(p(x_1|z_1))
+    for k in range(n_hstates):
+        omega[0,k] = log_initial_prob[k] + log_cond_prob[0,k]
+    # Reccursion
+    # (PRML 13.68) omega(z_{n+1}) = log(p(x_{n+1}|z_{n+1})) + max_{z_n}(log(p(x_{n+1}|z_n)) + omega(z_n))
+    for n in range(1, n_samples):
+        for k in range(n_hstates):
             for i in range(n_hstates):
-                for j in range(n_hstates):
-                    log_xi_sum[i, j] = c_logaddexp(log_xi_sum[i, j],
-                                                  work_buffer[i, j])
+                buffer[i] = log_transit_prob[i,k] + omega[n-1,i]
+            omega[n,k] = log_cond_prob[n,k] + c_max(buffer)
 
+    # TraceBack
+    # Because there is a unique path coming into that state we can trace the path
+    # back to step N-1 to see what state it occupied at that time, and so on back
+    # through the lattice to the state n=1.
+    ml_hstates[n_samples-1] = where_from = c_argmax(omega[n_samples-1])
+    cdef floating log_prob = omega[n_samples-1, where_from]
+    for n in range(n_samples-2, -1, -1):
+        for k in range(n_hstates):
+            buffer[k] = omega[n,k] + log_transit_prob[k,where_from]
+        ml_hstates[n] = where_from = c_argmax(buffer)
 
-def _viterbi(int n_samples, int n_hstates,
-             double[:] log_initial_prob,
-             double[:, :] log_transit_prob,
-             double[:, :] log_prob):
-
-    cdef int i, j, t, where_from
-    cdef double logprob
-
-    cdef int[::view.contiguous] state_sequence = \
-        np.empty(n_samples, dtype=np.int32)
-    cdef double[:, ::view.contiguous] viterbi_lattice = \
-        np.zeros((n_samples, n_hstates))
-    cdef double[::view.contiguous] work_buffer = np.empty(n_hstates)
-
-    with nogil:
-        for i in range(n_hstates):
-            viterbi_lattice[0, i] = log_initial_prob[i] + log_prob[0, i]
-
-        # Induction
-        for t in range(1, n_samples):
-            for i in range(n_hstates):
-                for j in range(n_hstates):
-                    work_buffer[j] = (log_transit_prob[j, i]
-                                      + viterbi_lattice[t - 1, j])
-
-                viterbi_lattice[t, i] = c_max(work_buffer) + log_prob[t, i]
-
-        # Observation traceback
-        state_sequence[n_samples - 1] = where_from = \
-            c_argmax(viterbi_lattice[n_samples - 1])
-        logprob = viterbi_lattice[n_samples - 1, where_from]
-
-        for t in range(n_samples - 2, -1, -1):
-            for i in range(n_hstates):
-                work_buffer[i] = (viterbi_lattice[t, i]
-                                  + log_transit_prob[i, where_from])
-
-            state_sequence[t] = where_from = c_argmax(work_buffer)
-
-    return np.asarray(state_sequence), logprob
+    return ml_hstates, log_prob

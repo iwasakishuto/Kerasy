4898174e80a4ad27aec5b42345c65b856f5a65d3
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-02-22 18:43:41 +0900

    Remain: monitor

--- kerasy/ML/__HMM.py
+++ kerasy/ML/__HMM.py
@@ -40,7 +40,6 @@ class BaseHMM(Params):
     @params init         : (str) 'path/to/params.json', or 'random', or (dict)
     @params algorithm    : (str) 'viterbi', or 'map'. How to decode the input state sequences.
     @params up_params    : (str) Which parameters are updated in the training process.
-    @params init_params  : (str) Which parameters are initialized prior to training.
     @params random_state : (int) random state for initialization.
     @attr initial   : Initial hidden states probabilities. shape=(n_hstates)
     @attr transit   : transition probabilities between hidden states. shape=(n_hstates, n_hstates)
@@ -76,70 +75,173 @@ class BaseHMM(Params):
 
     """
     def __init__(self, n_hstates=3, init="random", algorithm="viterbi",
-                 up_params=string.ascii_letters, init_params=string.ascii_letters,
-                 random_state=None):
+                 up_params=string.ascii_letters, random_state=None):
         super().__init__()
         self.n_hstates = n_hstates
         self.init = init
         self.algorithm = algorithm
         self.up_params = up_params
-        self.init_params = init_params
-        self.seed = random_state
-        self.disp_params = ["n_hstates", "init", "algorithm", "up_params", "init_params"]
+        self.rnd = handle_random_state(random_state)
+        self.disp_params = ["n_hstates", "init", "algorithm", "up_params"]
+        self.model_params = ["initial", "transit"]
 
     # @overrided
     def _check_params_validity():
-        pass
+        """ Validates model parameters.
+        This method checks `self.initial` and `self.transit` only, and
+        each sub class must override this method, as followings:
+        ```
+        def _check_params_validity():
+            super()._check_params_validity()
+            ...
+        ```
+        """
+        self.initial = initial = np.asarray(self.initial)
+        if initial.shape[0] != self.n_hstates:
+            raise ValueError(f"self.initial must have length n_hstates, ({initial.shape[0]} != {self.n_hstates})")
+        if not np.allclose(initial.sum(), 1.0):
+            raise ValueError(f"self.initial must sum to 1.0 (but got {initial.sum():.5f})")
+
+        self.transit = transit = np.asarray(self.transit)
+        if transit.shape != (self.n_hstates, self.n_hstates):
+            raise ValueError(f"self.transit must have shape (n_hstates, n_hstates)",
+                             f"({transit.shape} != {self.n_hstates,self.n_hstates})")
+        if not np.allclose(transit.sum(axis=1), 1.0):
+            raise ValueError(f"self.transit must sum to 1.0 (but got {transit.sum(axis=1)})")
+
+    def _init_params_by_json(self, json_path):
+        """ Initialize parameter by json. """
+        self.load_params(json_path)
+        remain_attrs = has_not_attrs(self, self.model_params)
+        if len(remain_attrs)>0:
+            for attr in remain_attrs:
+                warnings.warn(f"`{attr}` is not described in {json_path}, please specify it in the file",
+                              f"or self.{}=SOMETHING to define it.")
+
+    def _init_params_by_dict(self, params_dict):
+        """ Initialize parameter by dictionaly. """
+        self.__dict__.update(params_dict)
+        self.format_params(list_params=["disp_params"])
+        remain_attrs = has_not_attrs(self, self.model_params)
+        if len(remain_attrs)>0:
+            for attr in remain_attrs:
+                warnings.warn(f"`{attr}` is not described in dictionaly, please specify it in the file",
+                              f"or self.{}=SOMETHING to define it.")
+
+    def _init_params_by_random(self):
+        """ Initialize the parameters by random (only support for `initial` and `transit`). """
+        self.initial = self.rnd.rand(n_hstates)
+        normalize(self.initial)
+        self.transit = self.rnd.rand(self.n_hstates, self.n_hstates) + np.diag(np.ones(shape=self.n_hstates)*0.3)
+        normalize(self.transit, axis=1)
 
     # @overrided
     def _init_params(self, X, init):
-        """ Initialize the parameters """
-        self.n_states = self._check_and_get_n_states(X)
-        if init=="random":
-            rnd = np.random.RandomState(self.seed)
-            self.initial = rnd.rand(n_hstates)
-            normalize(self.initial)
-            self.transit = rnd.rand(self.n_hstates, self.n_hstates) + np.diag(np.ones(shape=self.n_hstates)*0.3)
-            normalize(self.transit, axis=1)
-            self.emission = rnd.rand(self.n_hstates, self.n_states)
-            normalize(self.emission)
-        elif os.path.exists(init):
-            self.load_params(init)
-            remain_attrs = has_not_attrs(self, "initial", "transit", "emission")
-            if len(remain_attrs)>0:
-                for attr in remain_attrs:
-                    warnings.warn(f"`{}` is not described in {init}, please specify it in the file, or self.{}=SOMETHING to define it.")
+        """
+        This method initialize parameters before training. If you initialize
+        parameters by random, it only handles with `initial`, and `transit`,
+        so each sub class must override this method, as followings.
+        ```
+        def _init_params(self, X, init):
+            self.hoge = self._check_and_get_hogehoge(X)
+            super()._init_params(X, init)
+
+            if isinstance(init, str) and init=="random":
+                ...
+        ```
+        ================================================
+        @params X    : Multiple connected samples. shape=(n_samples, n_features)
+        @params init : (str) 'path/to/params.json', or 'random', or (dict)
+        """
+        params_scale_dict = self._get_params_size()
+        n_trainable_params = sum([params_scale_dict[p] for p in self.up_params])
+        if X.size < n_trainable_params:
+            warnings.warn(f"Fitting a model with {n_trainable_params} trainable "
+                          f"parameters with only {X.size} data. It will result in "
+                          "a degenerate solution.")
+
+        if isinstance(init, dict):
+            self._init_params_by_dict(init)
+        elif isinstance(init, str):
+            if init=="random":
+                self._init_params_by_random()
+            else:
+                self._init_params_by_json(init)
         else:
             raise ValueError(f"the `init` parameter should be 'random', or 'path/to/json', but got {init})
 
-    # @pverrided
-    def _initialize_sufficient_statistics(self):
-        pass
+    # @overrided
+    def _init_statistics(self):
+        """ Initialize the statistics storage.
+        This method initialize the statistics storage. These information is used
+        to update the model parameters.
+        ```
+        def _init_statistics():
+            statistics = super()._init_statistics()
+            ...
+        ```
+        """
+        return {
+            "n_sample": 0,
+            "initial" : np.zeros(shape=(self.n_hstates)),
+            "transit" : np.zeros(shape=(self.n_hstates, self.n_hstates)),
+        }
 
     # @overrided
-    def _accumulate_sufficient_statistics(self, stats, X, framelogprob,
-                                              posteriors, fwdlattice, bwdlattice):
-        pass
+    def _update_statistics(self, statistics, X, log_cond_prob, posterior_prob, log_alpha, log_beta):
+        """ Update the statistics storage.
+        @params statistics     : (dict) statistics storage.
+        @params X              : Multiple connected samples. shape=(n_samples, n_features)
+        @params log_cond_prob  : Log conditional probabilities. shape=(n_samples, n_hstates)
+        @params posterior_prob : Posterior probabilities. shape=(n_samples, n_hstates)
+        @params log_alpha      : Log scaled alpha. shape=(n_samples, n_hstates)
+        @params log_beta       : Log scaled beta.  shape=(n_samples, n_hstates)
+        """
+        statistics['n_sample'] += 1
+        if 'i' in self.up_params:
+            statistics['initial'] += posterior_prob[0]
+        if 't' in self.up_params:
+            n_samples, n_components = log_cond_prob.shape
+            if n_samples>1:
+                log_xi_sum = c_hmm._compute_log_xi_sum(
+                    log_alpha, log_beta, log_mask_zero(self.transit), log_cond_prob
+                )
+                with np.errstate(under="ignore"):
+                    statistics['transit'] += np.exp(log_xi_sum)
+
+    @abstractmethod
+    def _get_params_size(self):
+        """
+        This method is used to check whether there is enough data or not.
+        Please refer to `_init_params` method for more details.
+        @return (dict) model paramete size per parameter.
+        """
+        raise NotImplemented("This class is Abstract.")
 
     @abstractmethod
     def _generate_sample(self, hstate, random_state=None):
-        """aa
-        """
+        """ Generate sample from given hidden state. """
         raise NotImplementedError("This class is Abstract")
 
     @abstractmethod
     def _compute_log_likelihood(self, X):
-        """ Computes per-component log probability under the model.
+        """ Computes log probability per hidden state under the model.
         @params X             : Multiple connected samples. shape=(n_samples, n_features)
         @params log_cond_prob : Log conditional probabilities. shape=(n_samples, n_hstates)
         """
         raise NotImplementedError("This class is Abstract")
 
     # @overrided
-    def _mstep(self):
+    def _Mstep(self, statistics):
         """ Mstep of Hidden Markov Model.
         """
-        raise NotImplementedError("This class is Abstract.")
+
+        if 's' in self.up_params:
+            self.initial = np.where(statistics['initial']==0, 0, statistics['initial'])
+            normalize(self.initial)
+        if 't' in self.up_params:
+            self.transit = np.where(statistics['transit']==0, 0, statistics['transit'])
+            normalize(self.transit, axis=1)
 
     def fit(self, X, lengths=None, max_iter=10, tol=1e-4, verbose=1):
         """ Baum-Welch Algorithm
@@ -148,22 +250,23 @@ class BaseHMM(Params):
         """
         self._init_params(X, self.init)
         self._check_params_validity()
+
         for it in range(max_iter):
+            statistics = self._init_statistics()
             current_log_prob = 0.
             for i,j in iter_from_X_lengths(X, lengths):
-                # log conditional probability (start with ith)
                 log_cond_prob_ij = self._compute_log_likelihood(X[i:j])
                 log_prob, log_alpha = self._Estep_log_forward(log_cond_prob_ij)
                 current_log_prob += log_prob
                 log_beta = self._Estep_log_backward(log_cond_prob_ij)
-                posteriors = self._compute_posteriors(log_alpha, bwdlattice)
-                # TODO: Update parameters.
-                # self._accumulate_sufficient_statistics(stats, X[i:j], log_cond_prob_ij, posteriors, log_alpha, log_beta)
+                posterior_prob = self._compute_posteriors(log_alpha, log_beta)
+                self._update_statistics(statistics, X[i:j], log_cond_prob_ij, posterior_prob, log_alpha, log_beta)
+
             flush_progress_bar(it, max_iter, metrics={"log probability": current_log_prob}, barname="Baum-Welch Algorithm")
-            self._mstep()
+            self._mstep(statistics)
+            
             if "CONDITION":
                 break
-        return
 
     def score_samples(self, X, length=None):
         """ Compute the posterior probability for each hidden state.
@@ -248,7 +351,7 @@ class BaseHMM(Params):
         @params random_state : (int) seed.
         """
         self._check_params_validity()
-        rnd = handle_random_state(self.seed if random_state is None else random_state)
+        rnd = handle_random_state(random_state) if random_state is not None else self.rnf
 
         transit = self.transit
         hidden_states = np.arange(self.n_hstates)
@@ -263,6 +366,7 @@ class BaseHMM(Params):
             samples.append(self._generate_sample(h_state, random_state=rnd))
             flush_progress_bar(n, n_samples, metrics={"hidden state": h_state})
 
+        self.rnd = rnd
         return np.atleast_2d(samples), np.asarray(h_state_sequences, dtype=int)
 
     def _viterbi(self, log_cond_prob):
@@ -317,11 +421,12 @@ class BaseHMM(Params):
 class MultinomialHMM(BaseHMM):
     """ Hidden Markov Model with multinomial (discrete) emissions """
     def __init__(self, n_hstates=3, init="random", algorithm="viterbi",
-                 up_params="ite", init_params="ite", random_state=None):
+                 up_params="ite", random_state=None):
         super().__init__(n_hstates=n_hstates, init=init, algorithm=algorithm,
-                         up_params=up_params, init_params=init_params,
+                         up_params=up_params,
                          random_state=random_state)
         self.disp_params.extend([])
+        self.model_params.extend(["emission"])
 
     def _check_and_get_n_states(self, X):
         """  """

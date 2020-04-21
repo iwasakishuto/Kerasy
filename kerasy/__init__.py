d85290a89d17c709210d167092b621a0fd4fb5fe
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-04-21 16:14:16 +0900

    the package contents visible from the top.

---
+++ kerasy/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+__version__ = '0.0.1'
+
+from . import Bio
+from . import clib
+from . import datasets
+from . import engine
+from . import layers
+from . import ML
+from . import search

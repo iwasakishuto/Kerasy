3e84dab1886931fc17669262e42283a17b29c62b
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-04-24 14:46:53 +0900

    Add __KERASY_SETUP__ variable.

--- kerasy/__init__.py
+++ kerasy/__init__.py
@@ -1,16 +1,26 @@
 # coding: utf-8
+import sys
 
 __copyright__    = "Copyright (C) 2020 Shuto Iwasaki"
-__version__      = "0.0.3"
+__version__      = "0.0.4"
 __license__      = "MIT"
 __author__       = "Shuto Iwasaki"
 __author_email__ = "cabernet.rock@gmail.com"
 __url__          = "https://iwasakishuto.github.io/Kerasy/doc"
 
-from . import Bio
-from . import clib
-from . import datasets
-from . import engine
-from . import layers
-from . import ML
-from . import search
+try:
+    __KERASY_SETUP__
+except NameError:
+    __KERASY_SETUP__ = False
+
+
+if __KERASY_SETUP__:
+    sys.stderr.write('Partial import of kerasy during the build process.\n')
+else:
+    from . import Bio
+    from . import clib
+    from . import datasets
+    from . import engine
+    from . import layers
+    from . import ML
+    from . import search

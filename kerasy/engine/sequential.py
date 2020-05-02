7fe40c3584552e2ff767878ba8b018e88ae34274
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-05-03 01:44:01 +0900

    Specialized Metrics for MNIST.

--- kerasy/engine/sequential.py
+++ kerasy/engine/sequential.py
@@ -10,16 +10,20 @@ from ..layers import Dropout
 
 from .. import optimizers
 from .. import losses
+from .. import metrics
 
 from ..utils import make_batches
 from ..utils import flush_progress_bar
 from ..utils import handleTypeError
 from ..utils import print_summary
 from ..utils import Table
+from ..utils import ProgressMonitor
+from ..utils import handle_random_state
 
 class Sequential():
-    def __init__(self):
+    def __init__(self, random_state=None):
         self.layers = []
+        self.rnd = handle_random_state(random_state)
 
     def add(self, layer):
         """Adds a layer instance."""
@@ -27,7 +31,7 @@ class Sequential():
             raise TypeError(f"The added layer must be an instance of class Layer. Found: {str(layer)}")
         self.layers.append(layer)
 
-    def compile(self, optimizer, loss=None, metrics=None):
+    def compile(self, optimizer, loss, metric):
         """ Creates the layer weights.
         @param optimizer: (String name of optimizer) or (Optimizer instance).
         @param loss     : (String name of loss function) or (Loss instance).
@@ -35,7 +39,7 @@ class Sequential():
         """
         self.optimizer = optimizers.get(optimizer)
         self.loss = losses.get(loss)
-        self.metrics = metrics
+        self.metrics = metrics.get(metric)
         input_layer = self.layers[0]
         handleTypeError(
             types=[Input], input_layer=input_layer,
@@ -49,45 +53,57 @@ class Sequential():
             x=None, y=None, batch_size=32, epochs=1, verbose=1, shuffle=True,
             validation_spilit=0, validation_data=None, validation_steps=None,
             class_weight=None, sample_weight=None, **kwargs):
-        if kwargs: raise TypeError(f'Unrecognized keyword arguments: {str(kwargs)}')
-        if (x is None) or (y is None): raise ValueError('Please specify the trainig data. (x,y)')
+        if kwargs:
+            raise TypeError(f'Unrecognized keyword arguments: {str(kwargs)}')
+        if (x is None) or (y is None):
+            raise ValueError('Please specify the trainig data. (x,y)')
         # Prepare validation data.
         do_validation = False
         if validation_data:
             do_validation = True
             if len(validation_data) == 2:
-                val_x, val_y = validation_data
+                x_val, y_val = validation_data
                 val_sample_weight = None
             elif len(validation_data) == 3:
-                val_x, val_y, val_sample_weight = validation_data
+                x_val, y_val, val_sample_weight = validation_data
             else:
                 raise ValueError(f"When passing validation_data, it must contain 2 (x_val, y_val) or 3 (x_val, y_val, val_sample_weights) items. However, it contains {len(validation)} items.")
+            num_val_samples = len(x_val)
 
         # Prepare for the trainig.
-        epoch_digit = len(str(epochs))
         num_train_samples = len(x)
         batches = make_batches(num_train_samples, batch_size)
         num_batchs = len(batches)
         index_array = np.arange(num_train_samples)
 
         for epoch in range(epochs):
-            if shuffle: np.random.shuffle(index_array)
+            if shuffle:
+                self.rnd.shuffle(index_array)
+            monitor = ProgressMonitor(
+                max_iter=num_batchs, verbose=verbose,
+                barname=f"Epoch {epoch+1:>0{len(str(epochs))}}/{epochs} |"
+            )
             losses = 0
             for batch_index, (batch_start, batch_end) in enumerate(batches):
                 batch_ids = index_array[batch_start:batch_end]
                 for bs, (x_train, y_true) in enumerate(zip(x[batch_ids], y[batch_ids])):
                     y_pred = self.forward_train(x_train)
-                    losses += self.loss.loss(y_true=y_true, y_pred=y_pred)
                     self.backprop(y_true=y_true, y_pred=y_pred)
+                    losses += self.metrics.loss(y_true=y_true, y_pred=np.argmax(y_pred))
                 self.updates(bs+1)
-                flush_progress_bar(
-                    it=batch_index, max_iter=num_batchs, verbose=verbose,
-                    barname=f"Epoch {epoch+1:>0{epoch_digit}}/{epochs} |",
-                    metrics={
-                        self.loss.name : losses/min((batch_index+1)*batch_size, num_train_samples)
-                    }
-                )
-            if verbose>=1: print()
+                # NOTE: This metrics is specialized for MNIST sample.
+                metrics = {
+                    self.metrics.name: f"{losses/min((batch_index+1)*batch_size, num_train_samples):.1%}",
+                }
+                monitor.report(it=batch_index, **metrics)
+            if do_validation:
+                # NOTE: This metrics is specialized for MNIST sample.
+                y_val_pred = self.predict(x_val)
+                metrics.update({
+                    "val_" + self.metrics.name: f"{self.metrics.loss(y_true=y_val, y_pred=np.argmax(y_val_pred, axis=1)):.1%}",
+                })
+                monitor.report(it=batch_index, **metrics)
+            monitor.remove()
 
     def forward_train(self, input):
         out=input

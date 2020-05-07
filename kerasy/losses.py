b962d62870a8773c05f29c54fa06e348329b4bee
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-05-07 18:05:30 +0900

    Add aggr_method and format_spec attributes.

--- kerasy/losses.py
+++ kerasy/losses.py
@@ -1,11 +1,19 @@
 # coding: utf-8
+import re
 import numpy as np
 from abc import ABCMeta, abstractmethod
 from .utils import mk_class_get
+from .utils import format_spec_create
 
 class KerasyAbstLoss(metaclass=ABCMeta):
-    def __init__(self):
-        self.name = self.__class__.__name__.lower()
+    def __init__(self, aggr_type="sum", fmt="3f", **format_codes):
+        self.name = re.sub(r"([a-z])([A-Z])", r"\1_\2", self.__class__.__name__).lower()
+        # Aggregation method for training.
+        self.aggr_method = {
+            "sum" : lambda sum,n : sum,
+            "ave" : lambda sum,n : sum/n
+        }.get(aggr_type, lambda sum,n : sum)
+        self.format_spec = format_spec_create(fmt=fmt, **format_codes)
 
     def __repr__(self):
         return f"{super().__repr__()}\n{self.__doc__}"
@@ -19,12 +27,19 @@ class KerasyAbstLoss(metaclass=ABCMeta):
         pass
 
 class MeanSquaredError(KerasyAbstLoss):
+    def __init__(self):
+        super().__init__(aggr_type="ave")
+
     def loss(self, y_true, y_pred):
         return np.mean(np.square(y_pred - y_true), axis=-1)
     def diff(self, y_true, y_pred):
         return y_pred-y_true
 
 class CategoricalCrossentropy(KerasyAbstLoss):
+    """
+    Categorical CrossEntropy Loss: np.sum(y_true * np.log(y_pred))
+    Cross-entropy loss increases as the predicted probability diverges from the actual label.
+    """
     def loss(self, y_true, y_pred):
         small_val = 1e-7 # Avoiding np.log(y_pred)=inf if y_pred==0
         return -np.sum(y_true * np.log(y_pred+small_val))
--- kerasy/metrics.py
+++ kerasy/metrics.py
@@ -1,13 +1,21 @@
 # coding: utf-8
+import re
 import numpy as np
 from abc import ABCMeta, abstractmethod
 from .utils import mk_class_get
+from .utils import format_spec_create
 
 from .losses import KerasyAbstLoss, KerasyLossClasses
 
 class KerasyAbstMetrics(metaclass=ABCMeta):
-    def __init__(self):
-        self.name = self.__class__.__name__.lower()
+    def __init__(self, aggr_type="sum", fmt="3f", **format_codes):
+        self.name = re.sub(r"([a-z])([A-Z])", r"\1_\2", self.__class__.__name__).lower()
+        # Aggregation method for training.
+        self.aggr_method = {
+            "sum" : lambda sum,n : sum,
+            "ave" : lambda sum,n : sum/n
+        }.get(aggr_type, lambda sum,n : sum)
+        self.format_spec = format_spec_create(fmt=fmt, **format_codes)
 
     def __repr__(self):
         return f"{super().__repr__()}\n{self.__doc__}"
@@ -16,13 +24,23 @@ class KerasyAbstMetrics(metaclass=ABCMeta):
     def loss(self, y_true, y_pred):
         pass
 
-class Accuracy(KerasyAbstMetrics):
+class CategoricalAccuracy(KerasyAbstMetrics):
+    """
+    Categorical Accuracy:
+    Calculates the mean accuracy rate across all predictions for multiclass classification problems.
+    """
+    def __init__(self):
+        super().__init__(aggr_type="ave", fmt=".1%")
+
     def loss(self, y_true, y_pred):
-        return np.mean(y_true==y_pred)
+        return np.mean(np.equal(
+            y_true.argmax(axis=-1),
+            y_pred.argmax(axis=-1)
+        ))
+
 
 KerasyMetricsClasses = {
-    'acc'      : Accuracy,
-    'accuracy' :  Accuracy,
+    'categorical_accuracy': CategoricalAccuracy
 }
 KerasyMetricsClasses.update(KerasyLossClasses)
 

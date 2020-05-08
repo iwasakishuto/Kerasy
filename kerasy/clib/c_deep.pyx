fdbe72cb74a537037985f50fdc8b018025568ccf
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-05-08 20:56:45 +0900

    C++ conv2D for-loop (not improved.)

--- kerasy/clib/c_deep.pyx
+++ kerasy/clib/c_deep.pyx
@@ -10,6 +10,50 @@ from cython cimport floating
 from libc.math cimport sqrt
 from libc.stdio cimport printf
 
+ctypedef np.float64_t DOUBLE
+
+cdef int _backprop_mask(int i,j,m,n,sh,sw,kh,kw,OH,OW):
+    # Kernel IndexError Handler & Backprop IndexError Handler
+    return (i%sh+m<kh and j%sw+n<kw) and ((OH > (i-m)//sh >= 0) and (OW > (j-n)//sw >= 0))
+
+
+def Conv2D_forward(
+    np.ndarray[DOUBLE, ndim=3, mode='c'] a,
+    np.ndarray[DOUBLE, ndim=3, mode='c'] Xin,
+    np.ndarray[DOUBLE, ndim=4, mode='c'] kernel,
+    int OH, OW, sh, sw, kh, kw):
+
+    cdef int i,j
+    for i in range(OH//sh):
+        for j in range(OW//sw):
+            a[i,j,:] = np.sum(Xin[sh*i:(sh*i+kh), sw*j:(sw*j+kw), :, None]*kernel, axis=(0,1,2))
+
+def Conv2D_backprop(
+    np.ndarray[DOUBLE, ndim=3, mode='c'] dEda,
+    np.ndarray[DOUBLE, ndim=3, mode='c'] dEdXin,
+    np.ndarray[DOUBLE, ndim=4, mode='c'] dEdw,
+    np.ndarray[DOUBLE, ndim=3, mode='c'] Xin,
+    np.ndarray[DOUBLE, ndim=4, mode='c'] kernel,
+    int padH, padW, F, OH, OW, OF, sh, sw, kh, kw, trainable=1):
+
+    cdef int i,j,c
+    for c in range(F):
+        for i in range(padH):
+            for j in range(padW):
+                dEdXin[i,j,c] = np.mean([
+                    dEda[(i-m)//sh, (j-n)//sw, :] * kernel[i%sh+m, j%sw+n, c, :] \
+                    for m in range(0,kh,sh) \
+                    for n in range(0,kw,sw) \
+                    if _backprop_mask(i,j,m,n,sh,sw,kh,kw,OH,OW)
+                ])
+
+    if trainable:
+        for m in range(kh):
+            for n in range(kw):
+                for c in range(F):
+                    for c_ in range(OF):
+                        dEdw[m,n,c,c_] = np.sum(dEda[:,:,c_] * Xin[m:m+OH:sh, n:n+OW:sw, c])
+
 def conv2D_forward_gil(
         np.ndarray[floating, ndim=3, mode='c'] Xin,
         np.ndarray[floating, ndim=3, mode='c'] Xout,

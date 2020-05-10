5572a730d5c1cabfc9811c5ec6c6628044281b4c
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-05-10 14:56:33 +0900

    Change from function to callable class.

--- kerasy/initializers.py
+++ kerasy/initializers.py
@@ -1,127 +1,153 @@
 # coding: utf-8
 import numpy as np
 from scipy import stats
+from abc import ABCMeta, abstractmethod
 
+from .utils import mk_class_get
 from .utils import handleKeyError
+from .utils import handleRandomState
 
-def Zeros(shape, dtype=None):
-    return np.zeros(shape=shape, dtype=dtype)
+class KerasyAbstInitializer(metaclass=ABCMeta):
+    def __init__(self, aggr_type="sum", fmt=".3f", **format_codes):
+        self.name = re.sub(r"([a-z])([A-Z])", r"\1_\2", self.__class__.__name__).lower()
 
-def Ones(shape, dtype=None):
-    return np.ones(shape=shape, dtype=dtype)
+    @abstractmethod
+    def __call__(self, shape, dtype=None):
+        raise NotImplementedError
 
-def Constant(shape, value=0, dtype=None):
-    return np.full(shape=shape, fill_value=value, dtype=dtype)
+class Zeros(KerasyAbstInitializer):
+    def __call__(self, shape, dtype=None):
+        return np.zeros(shape=shape, dtype=dtype)
 
-def RandomNormal(shape, mean=0, stddev=0.05, dtype=None, seed=None):
-    rng = np.random.RandomState(seed) if seed is not None else np.random
-    return rng.normal(size=shape, loc=mean, scale=stddev).astype(dtype)
+class Ones(KerasyAbstInitializer):
+    def __call__(self, shape, dtype=None):
+        return np.ones(shape=shape, dtype=dtype)
 
-def RandomUniform(shape, minval=-0.05, maxval=0.05, dtype=None, seed=None):
-    rng = np.random.RandomState(seed) if seed is not None else np.random
-    return rng.uniform(size=shape, low=minval, high=maxval)
+class Constant(KerasyAbstInitializer):
+    def __call__(self, shape, value=0, dtype=None):
+        return np.full(shape=shape, fill_value=value, dtype=dtype)
 
-def TruncatedNormal(shape, mean=0.0, stddev=0.05, dtype=None, seed=None):
-    X = stats.truncnorm(
-        (-stddev - mean) / stddev,
-        (stddev  - mean) / stddev,
-        loc=mean,
-        scale=stddev,
-    )
-    return X.rvs(size=shape,random_state=seed).astype(dtype)
+class RandomNormal(KerasyAbstInitializer):
+    def __call__(self, shape, mean=0, stddev=0.05, dtype=None, seed=None):
+        rnd = handleRandomState(seed)
+        return rnd.normal(size=shape, loc=mean, scale=stddev).astype(dtype)
 
-def VarianceScaling(shape, scale=1.0, mode='fan_in', distribution='normal', dtype=None, seed=None):
-    fan_in, fan_out = _compute_fans(shape)
-    n = fan_in if mode=="fan_in" else fan_out if mode=="fan_out" else (fan_in+fan_out)/2
-    scale /= max(1., n)
-    if distribution=='normal':
-        # 0.879... = scipy.stats.truncnorm.std(a=-2, b=2, loc=0., scale=1.)
-        stddev = np.sqrt(scale) / .87962566103423978
-        return TruncatedNormal(shape=shape, mean=0.0, stddev=stddev, dtype=dtype, seed=seed)
-    else:
-        limit = np.sqrt(3 * scale)
-        return RandomUniform(shape=shape, minval=-limit, maxval=limit, dtype=dtype, seed=seed)
+class RandomUniform(KerasyAbstInitializer):
+    def __call__(self, shape, minval=-0.05, maxval=0.05, dtype=None, seed=None):
+        rnd = handleRandomState(seed)
+        return rnd.uniform(size=shape, low=minval, high=maxval)
 
-def Orthogonal(shape, gain=1.0, dtype=None, seed=None):
-    num_rows = 1
-    for dim in shape[:-1]:
-        num_rows *= dim
-    num_cols = shape[-1]
-    flat_shape = (num_rows, num_cols)
-    rng = np.random
-    if seed is not None:
-        rng = np.random.RandomState(seed)
-    a = rng.normal(loc=0.0, scale=1.0, size=flat_shape).astype(dtype)
-    u, _, v = np.linalg.svd(a, full_matrices=False)
-    # Pick the one with the correct shape.
-    q = u if u.shape == flat_shape else v
-    q = q.reshape(shape)
-    return gain * q[:shape[0], :shape[1]]
+class TruncatedNormal(KerasyAbstInitializer):
+    def __call__(self, shape, mean=0.0, stddev=0.05, dtype=None, seed=None):
+        X = stats.truncnorm(
+            (-stddev - mean) / stddev,
+            (stddev  - mean) / stddev,
+            loc=mean,
+            scale=stddev,
+        )
+        return X.rvs(size=shape,random_state=seed).astype(dtype)
 
-def Identity(shape, dtype=None, gain=1.0):
-    if len(shape) != 2 or shape[0]!=shape[1]:
-        raise ValueError('Identity matrix initializer can only be used for 2D Square matrices.')
-    return gain * np.eye(N=shape[0], dtype=dtype)
+class VarianceScaling(KerasyAbstInitializer):
+    def __call__(self, shape, scale=1.0, mode='fan_in', distribution='normal', dtype=None, seed=None):
+        fan_in, fan_out = _compute_fans(shape)
+        n = fan_in if mode=="fan_in" else fan_out if mode=="fan_out" else (fan_in+fan_out)/2
+        scale /= max(1., n)
+        if distribution=='normal':
+            # 0.879... = scipy.stats.truncnorm.std(a=-2, b=2, loc=0., scale=1.)
+            stddev = np.sqrt(scale) / .87962566103423978
+            return TruncatedNormal(shape=shape, mean=0.0, stddev=stddev, dtype=dtype, seed=seed)
+        else:
+            limit = np.sqrt(3 * scale)
+            return RandomUniform(shape=shape, minval=-limit, maxval=limit, dtype=dtype, seed=seed)
 
-def GlorotNormal(shape, dtype=None, seed=None):
-    return VarianceScaling(
-        shape=shape,
-        scale=1.,
-        mode='fan_avg',
-        distribution='normal',
-        dtype=dtype,
-        seed=seed
-    )
+class Orthogonal(KerasyAbstInitializer):
+    def __call__(self, shape, gain=1.0, dtype=None, seed=None):
+        num_rows = 1
+        for dim in shape[:-1]:
+            num_rows *= dim
+        num_cols = shape[-1]
+        flat_shape = (num_rows, num_cols)
+        rng = np.random
+        if seed is not None:
+            rng = np.random.RandomState(seed)
+        a = rng.normal(loc=0.0, scale=1.0, size=flat_shape).astype(dtype)
+        u, _, v = np.linalg.svd(a, full_matrices=False)
+        # Pick the one with the correct shape.
+        q = u if u.shape == flat_shape else v
+        q = q.reshape(shape)
+        return gain * q[:shape[0], :shape[1]]
 
-def GlorotUniform(shape, dtype=None, seed=None):
-    return VarianceScaling(
-        shape=shape,
-        scale=1.,
-        mode='fan_avg',
-        distribution='uniform',
-        dtype=dtype,
-        seed=seed
-    )
+class Identity(KerasyAbstInitializer):
+    def __call__(self, shape, dtype=None, gain=1.0):
+        if len(shape) != 2 or shape[0]!=shape[1]:
+            raise ValueError('Identity matrix initializer can only be used for 2D Square matrices.')
+        return gain * np.eye(N=shape[0], dtype=dtype)
 
-def HeNormal(shape, dtype=None, seed=None):
-    return VarianceScaling(
-        shape=shape,
-        scale=2.,
-        mode='fan_in',
-        distribution='normal',
-        dtype=dtype,
-        seed=seed
-    )
+class GlorotNormal(KerasyAbstInitializer):
+    def __call__(self, shape, dtype=None, seed=None):
+        return VarianceScaling(
+            shape=shape,
+            scale=1.,
+            mode='fan_avg',
+            distribution='normal',
+            dtype=dtype,
+            seed=seed
+        )
 
-def LeCunNormal(shape, dtype=None, seed=None):
-    return VarianceScaling(
-        shape=shape,
-        scale=1.,
-        mode='fan_in',
-        distribution='normal',
-        dtype=dtype,
-        seed=seed
-    )
+class GlorotUniform(KerasyAbstInitializer):
+    def __call__(self, shape, dtype=None, seed=None):
+        return VarianceScaling(
+            shape=shape,
+            scale=1.,
+            mode='fan_avg',
+            distribution='uniform',
+            dtype=dtype,
+            seed=seed
+        )
 
-def HeUniform(shape, dtype=None, seed=None):
-    return VarianceScaling(
-        shape=shape,
-        scale=2.,
-        mode='fan_in',
-        distribution='uniform',
-        dtype=dtype,
-        seed=seed
-    )
+class HeNormal(KerasyAbstInitializer):
+    def __call__(self, shape, dtype=None, seed=None):
+        return VarianceScaling(
+            shape=shape,
+            scale=2.,
+            mode='fan_in',
+            distribution='normal',
+            dtype=dtype,
+            seed=seed
+        )
 
-def LeCunUniform(shape, dtype=None, seed=None):
-    return VarianceScaling(
-        shape=shape,
-        scale=1.,
-        mode='fan_in',
-        distribution='uniform',
-        dtype=dtype,
-        seed=seed
-    )
+class LeCunNormal(KerasyAbstInitializer):
+    def __call__(self, shape, dtype=None, seed=None):
+        return VarianceScaling(
+            shape=shape,
+            scale=1.,
+            mode='fan_in',
+            distribution='normal',
+            dtype=dtype,
+            seed=seed
+        )
+
+class HeUniform(KerasyAbstInitializer):
+    def __call__(self, shape, dtype=None, seed=None):
+        return VarianceScaling(
+            shape=shape,
+            scale=2.,
+            mode='fan_in',
+            distribution='uniform',
+            dtype=dtype,
+            seed=seed
+        )
+
+class LeCunUniform(KerasyAbstInitializer):
+    def __call__(self, shape, dtype=None, seed=None):
+        return VarianceScaling(
+            shape=shape,
+            scale=1.,
+            mode='fan_in',
+            distribution='uniform',
+            dtype=dtype,
+            seed=seed
+        )
 
 def _compute_fans(shape, data_format='channels_last'):
     """Computes the number of input and output units for a weight shape.
@@ -153,33 +179,25 @@ def _compute_fans(shape, data_format='channels_last'):
     return fan_in, fan_out
 
 KerasyInitializerFunctions = {
-    'zeros': Zeros,
-    'ones': Ones,
-    'constant': Constant,
-    'random_normal': RandomNormal,
-    'random_uniform': RandomUniform,
-    'truncated_normal': TruncatedNormal,
-    'variance_scaling': VarianceScaling,
-    'orthogonal': Orthogonal,
-    'identity': Identity,
-    'glorot_normal': GlorotNormal,
-    'glorot_uniform': GlorotUniform,
-    'he_normal': HeNormal,
-    'lecun_normal': LeCunNormal,
-    'he_uniform': HeUniform,
-    'lecun_uniform': LeCunUniform,
+    'zeros'            : Zeros,
+    'ones'             : Ones,
+    'constant'         : Constant,
+    'random_normal'    : RandomNormal,
+    'random_uniform'   : RandomUniform,
+    'truncated_normal' : TruncatedNormal,
+    'variance_scaling' : VarianceScaling,
+    'orthogonal'       : Orthogonal,
+    'identity'         : Identity,
+    'glorot_normal'    : GlorotNormal,
+    'glorot_uniform'   : GlorotUniform,
+    'he_normal'        : HeNormal,
+    'lecun_normal'     : LeCunNormal,
+    'he_uniform'       : HeUniform,
+    'lecun_uniform'    : LeCunUniform,
 }
 
-def get(identifier):
-    """
-    Retrieves a Kerasy Initializer function.
-    @params identifier : Initializer function, string name of a initializer, or
-                         a Kerasy Initializer function.
-    @return initializer: A Kerasy Initializer function.
-    """
-    if isinstance(identifier, str):
-        handleKeyError(lst=list(KerasyInitializerFunctions.keys()), identifier=identifier)
-        func = KerasyInitializerFunctions.get(identifier)
-    else:
-        handleTypeError(types=[str], identifier=identifier)
-    return func
+get = mk_class_get(
+    all_classes=KerasyInitializerFunctions,
+    kerasy_abst_class=[KerasyAbstInitializer],
+    genre="initializer"
+)

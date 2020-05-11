3c1ec1c169f8bfcdd766bd3e07df59b6670fb144
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-05-11 11:04:08 +0900

    Maxsets detection test.

--- tests/Bio/test_maxsets.py
+++ tests/Bio/test_maxsets.py
@@ -1,1 +1,28 @@
 # coding: utf-8
+import numpy as np
+from kerasy.utils import generateSeq
+from kerasy.Bio.factorization import simple_compression, simple_decompression
+
+len_array = 1000
+
+def get_test_data():
+    rnd = np.random.RandomState(123)
+    R_int   = rnd.randint(low=-10, high=100, size=len_array)
+    R_float = rnd.uniform(low=-10, high=100, size=len_array)
+    return R_int, R_float
+
+def _test_maxsets(R, limits=(5,10)):
+    model = MSS()
+    for i,limit in enumerate(sorted(limits)):
+        model.run(R, limit=limit, verbose=-1)
+        score = model.score
+        assert i==0 or score >= prev_score
+        prev_score = score
+
+def test_maxsets_int():
+    R_int, _ = get_test_data()
+    _test_maxsets(R_int, limits=(5,10))
+
+def test_maxsets_float():
+    _, R_float = get_test_data()
+    _test_maxsets(R_float, limits=(5,10))
